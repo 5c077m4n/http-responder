61a16f3a680bcb5faae4b6766a8d1ee173ddc021
Roee <roy.shap1@gmail.com>
2019-01-27 22:17:41 +0200

    Updated tests.

--- test/dist.js
+++ test/dist.js
@@ -87,7 +87,7 @@ describe('The camelcase function', function () {
 	const testStrings = ['i am a teapot', 'I am a  teapot', 'i aM a teapot', 'i am a teaPot', 'I aM A teApoT', 'I aM A TeAPOT', 'I-aM~A`TeAPOT'];
 
 	testStrings.forEach(testString =>
-		it(`should camelcase the sentence "${testString}"`, function () {
+		it(`should camelcase the sentence "${testString}" => "${correctStr}"`, function () {
 			expect(camelcase(testString)).to.deep.equal(correctStr);
 		})
 	);
