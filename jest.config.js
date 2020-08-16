37c1cc8d60f935a161a54a3696a9f9ca1629503d
Roee Shapira <roy.shap1@gmail.com>
2020-08-17 00:12:31 +0300

    Improved testing.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
 	},
-	testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.ts$',
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
 	moduleFileExtensions: ['ts', 'js'],
 };
